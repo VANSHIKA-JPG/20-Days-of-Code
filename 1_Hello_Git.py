81cd06cfc3e9e48736a324f10f63837b880427cc
Vanshika Bhutani <vanshikabhutani001@gmail.com>
2025-07-03 13:35:59 +0530

    hello

---
+++ 1_Hello_Git.py
@@ -0,0 +1,9 @@
+def intro():
+    Name=input("Enter your Name: ")
+    Age=int(input("Enter your Age: "))
+    Interests=input("Enter your Interests: ")
+    Areas_Of_Exploring=input("Enter your area of interests : ")
+    print("\n  MY INTRODUCTION ")              
+    print(f"Hi , I'm {Name} - a {Age} -year-old passionate Computer Science Engineering (CSE) student with a strong interest in {Interests}.I'm currently exploring areas like {Areas_Of_Exploring} I believe in continuous learning and enjoy working on meaningful projects that can create real impact. I'm eager to grow my skills, collaborate with like-minded individuals, and contribute to the tech community." )              
+intro()    
+              
